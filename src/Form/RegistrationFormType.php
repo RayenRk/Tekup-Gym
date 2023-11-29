6a5d5d4f27ea2eb06b1bd5d3cda921a41248c795
firas.houaidi@tek-up.de firas houaidi <houaidifiras.tekup@gmail.com>
2023-11-29 16:02:30 +0100

    ajout user role

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\UserSECURITY;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -17,7 +18,18 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('roles', ChoiceType::class, [
+                'choices' => [
+                    'Coach' => 'ROLE_USER',
+                    'Adherant' => 'ROLE_ADMIN',
+                    // Ajoutez d'autres rôles si nécessaire
+                ],
+                'multiple' => true, // Permettre à l'utilisateur de choisir plusieurs rôles
+                'required' => true,
+            ])
+
+
+        ->add('email')
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
